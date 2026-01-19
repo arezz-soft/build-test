44b94043f84748b8223f273b681845180ee49e78
Arez <aramdlersss@gmail.com>
2026-01-19 13:06:36 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,9 @@ return (
     <div className="page-container">
       <Head>
         <title>Build~Computers </title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/icons/icon-16.png" sizes="16x16" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
         <link rel="icon" href="/icons/icon-24.png" sizes="24x24" />
         <link rel="icon" href="/icons/icon-32.png" sizes="32x32" />
         <link rel="icon" href="/icons/icon-64.png" sizes="64x64" />
@@ -123,8 +125,23 @@ return (
         <link rel="icon" href="/icons/icon-192.png" sizes="192x192" />
         <link rel="icon" href="/icons/icon-512.png" sizes="512x512" />
         <link rel="apple-touch-icon" href="/icons/icon-180.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileImage" content="/icons/icon-512.png" />
+
+        {/* Open Graph Meta Tags for Social Previews */}
+        <meta property="og:title" content="Build~Computers" />
+        <meta property="og:description" content="Choose the best components and create your perfect setup. Created by Arez & Hasty." />
+        <meta property="og:image" content="/icons/icon-512.png" />
+        <meta property="og:url" content="https://build-computer.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Build~Computers" />
+        <meta name="twitter:description" content="Choose the best components and create your perfect setup." />
+        <meta name="twitter:image" content="/icons/icon-512.png" />
       </Head>
+
 
       <header className="main-header">
         <div className="header-logo" onClick={() => router.push('/')}>
@@ -236,3 +253,4 @@ return (
     </div>
   );
 }
+
